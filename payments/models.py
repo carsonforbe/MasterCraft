fdeec1ff65436a1ac683e9251749b6bf1972a1a5
Charles <105591024+carsonforbe@users.noreply.github.com>
2025-06-07 05:27:21 -0400

    Update models.py

--- payments/models.py
+++ payments/models.py
@@ -11,7 +11,7 @@ class Payment(models.Model):
     customer_name = models.CharField(max_length=200)
     customer_email = models.EmailField()
     amount = models.DecimalField(max_digits=10, decimal_places=2)
-    status = models.CharField()
+    status = models.CharField(blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateField(auto_now_add=True)
     gateaway_reference = models.CharField(max_length=255, blank=True)
